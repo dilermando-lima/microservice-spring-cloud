581be29c7754ef7b5f667631da3943cef39adddf
dilo <dilermando.silva@ssys.com.br>
2021-07-02 11:15:37 -0300

    unified all env vars for dockers and debug code

--- docker-compose-up-all-deps.sh
+++ docker-compose-up-all-deps.sh
@@ -5,12 +5,18 @@
 echo -e "\nDo you wish run 'docker-compose up' to all services in container/dependencies/docker-compose (Y/N)?"
 read CONTINUE_DWNLD;
 
-if [ "$CONTINUE_DWNLD" == "Y" ]; then
+if [ "$CONTINUE_DWNLD" == "Y" ] || [ "$CONTINUE_DWNLD" == "y" ]  ; then
 
-   docker-compose -f container/dependencies/docker-compose.yml down # --remove-orphans
-   docker-compose -f container/dependencies/docker-compose.yml up -d # --build --force-recreate
+   docker-compose  \
+        --env-file ./../../props/local.env  \
+        --file ./container/dependencies/docker-compose.yml down # --remove-orphans
+   
+   docker-compose \
+        --env-file ./../../props/local.env \
+        --file ./container/dependencies/docker-compose.yml up -d # --build --force-recreate
 
     echo -e "\nAll services are up."
+
 else
     echo -e "\nCommand is aborted."
 fi
